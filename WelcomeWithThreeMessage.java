881c07df0b4c2e3aeeca5fde2d102372b0a8e1d2
ly0205 <what2do1999@gmail.com>
2018-09-08 17:42:13 +0800

    wrote three files

---
+++ WelcomeWithThreeMessage.java
@@ -0,0 +1,8 @@
+public class WelcomeWithThreeMessage {
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals first");
+        System.out.println("Problem driven");
+    
+    }
+}
