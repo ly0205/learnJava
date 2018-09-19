e91f6bb34e9592d3100ac25db9dae22639b19c14
ly0205 <what2do1999@gmail.com>
2018-09-19 18:41:14 +0800

    println to print

--- ComputeAreaWithConsoleInput.java
+++ ComputeAreaWithConsoleInput.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ComputeAreaWithConsoleInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number for radius: ");
+        System.out.print("Enter a number for radius: ");
         double radius = input.nextDouble();
         double area = radius * radius * 3.14159;
         System.out.println("The area for the circle of" + radius + " is " + area );
